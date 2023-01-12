31b4dcbcfc3d220f43ba6cce97fef3e5d4430763
Muhammad Maaz Azhar <maazazher28@gmail.com>
2023-01-12 11:40:41 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,84 +1,98 @@
 let memo = {};
 
-let x = 2;
+let x = 3;
 let y = [];
+let avgCount = 5;
 
-for (let i = 0; i < 500; i++) {
-    y.push(i);
+for (let i = 0; i < 12; i++) {
+  y.push(i);
 }
 
 let ans = document.getElementById("ans");
 
 function ack(x, y) {
-    if (x === 0) {
-        return y + 1;
-    } else if (y === 0) {
-        return ack(x - 1, 1);
+  if (x === 0) {
+    return y + 1;
+  } else if (y === 0) {
+    return ack(x - 1, 1);
+  } else {
+    let key = x + "," + y;
+    if (key in memo) {
+      return memo[key];
     } else {
-        let key = x + ',' + y;
-        if (key in memo) {
-            return memo[key];
-        } else {
-            memo[key] = ack(x - 1, ack(x, y - 1));
-            return memo[key];
-        }
+      memo[key] = ack(x - 1, ack(x, y - 1));
+      return memo[key];
     }
+  }
 }
 
-const calculateTime = (x,y) => {
+const calculateTime = (x, y) => {
+  let sum = 0;
+  for (let i = 0; i < avgCount; i++) {
     let start = performance.now();
-    ack(x,y);
+    ack(x, y);
     let end = performance.now();
     let time = end - start;
-    return time
-}
+    sum += time;
+  }
 
-const PlotChart = (FinalAverage)=>{
-    const ctx = document.getElementById('myChart');
+  return sum / avgCount;
+};
 
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: y,
-            datasets: [{
-                label: 'Average Time of Execution with Respect to Y',
-                data: FinalAverage,
-                fill: false,
-                borderColor: 'rgb(75, 192, 192)',
-                tension: 0.1
-            }]
+const PlotChart = (FinalAverage) => {
+  const ctx = document.getElementById("myChart");
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: y,
+      datasets: [
+        {
+          label: "Average Time",
+          data: FinalAverage,
+          fill: false,
+          borderColor: "rgb(75, 192, 192)",
+          tension: 0.1,
+          yAxisID: 'y'
         },
-        options: {
-        scales: {
-            y: {
-            beginAtZero: true
-            }
+        {
+            label: "Iteration",
+            data: y,
+            fill: false,
+            borderColor: "#e74c3c",
+            tension: 0.1,
+            yAxisID: 'y1'
         }
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+        y1: {
+            beginAtZero: true,
         }
-    });
-}
+      },
+    },
+  });
+};
 
-window.onload = ()=>{
-    let FinalAverage = [];
-    for (let i = 0; i < y.length; i++) {
-        let AvgTime = [];
-        for(let i=0; i < 3; i++){
-            AvgTime.push(calculateTime(x,y[i]));
-        }
-        let Avg = 0;
-        for (const element of AvgTime) {
-            Avg += element;
-        }
-        FinalAverage.push(Avg/3)
-            
-        ans.innerHTML += `
+window.onload = () => {
+  let Average = [];
+  for (let i = 0; i < y.length; i++) {
+    let Avg = calculateTime(x,i);
+
+    Average.push(Avg);
+
+    ans.innerHTML += `
             <tr>
                 <td>${i}</td>
                 <td>${x}</td>
                 <td>${y[i]}</td>
-                <td>${Avg/3}</td>
+                <td>${Avg}</td>
             </tr>
-        `;        
-    }
-    PlotChart(FinalAverage);
+        `;
+  }
+  PlotChart(Average);
 };
